6e33755c2c2a49083ba44385436776f7f950aaba
liamvwoudenberg <liamvwoudenberg@gmail.com>
2025-12-04 19:31:03 +0000

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        gray: {
+          750: '#2d3748',
+          850: '#1a202c',
+          950: '#0d1117',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
