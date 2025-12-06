2738e69bb8d1f546e82255d944896d6274ebddbf
liamvwoudenberg <liamvwoudenberg@gmail.com>
2025-12-06 19:06:14 +0000

    poop

--- Editor.tsx
+++
@@ -1,6 +0,0 @@
-<div className="h-screen bg-slate-950 text-slate-100 flex flex-col overflow-hidden font-sans">
-  <Header user={user} onSignOut={onSignOut} onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} onPresent={() => setIsPresenting(true)} onBackToDashboard={() => navigate('/')} onShare={() => {}} isSidebarOpen={isSidebarOpen} />
-  <div className="flex-1 flex overflow-hidden">
-    <main className="flex-1 overflow-y-auto w-full p-4 sm:p-6 lg:p-10 scroll-smooth"></main>
-  </div>
-</div>
